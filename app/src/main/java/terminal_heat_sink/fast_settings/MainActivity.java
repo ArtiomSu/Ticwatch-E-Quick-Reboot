86dc3f42a988af8c2df8d51a9575cec27f974fc0
ArtiomSu <artiom.sumigora@mycit.ie>
2020-07-19 18:25:08 +0100

    build fixes

--- app/src/main/java/terminal_heat_sink/fast_settings/MainActivity.java
+++ app/src/main/java/terminal_heat_sink/fast_settings/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends WearableActivity {
 
