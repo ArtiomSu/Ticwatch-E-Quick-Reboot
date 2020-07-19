ac3f8a3e42f6cb1b3e371d2162d4e92b24c4d697
ArtiomSu <artiom.sumigora@mycit.ie>
2020-07-19 18:07:30 +0100

    initial commit

---
+++ app/src/main/java/terminal_heat_sink/fast_settings/MainActivity.java
@@ -0,0 +1,77 @@
+package terminal_heat_sink.fast_settings;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends WearableActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView power_off = (ImageView) findViewById(R.id.button_off);
+
+        power_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Process proc = Runtime.getRuntime()
+                            .exec(new String[]{ "su", "-c", "reboot -p" });
+                    proc.waitFor();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+
+        ImageView reboot = (ImageView) findViewById(R.id.button_reboot);
+        reboot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Process proc = Runtime.getRuntime()
+                            .exec(new String[]{ "su", "-c", "reboot" });
+                    proc.waitFor();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        ImageView reboottwrp = (ImageView) findViewById(R.id.button_twrp);
+        reboottwrp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Process proc = Runtime.getRuntime()
+                            .exec(new String[]{ "su", "-c", "reboot recovery" });
+                    proc.waitFor();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        ImageView rebootbootloader = (ImageView) findViewById(R.id.button_bootloader);
+        rebootbootloader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Process proc = Runtime.getRuntime()
+                            .exec(new String[]{ "su", "-c", "reboot bootloader" });
+                    proc.waitFor();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
